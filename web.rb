b493919eba021af689159a397fa4d05febef5cad
Johnathan Croom <johnathancroom@gmail.com>
2012-06-25 21:47:45 -0700

    Started sass compiling + basic layout

--- web.rb
+++ web.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'sass'
 
 get '/' do
-  "Hello, world"
+  erb :index
+end
+
+post '/' do
+  input = 'lighten(#333, 10%)' # Input value
+  input = params[:code]
+  
+  if input != ''
+    prefix = 'body{color:'
+    suffix = '}'
+    
+    compressed_result = Sass.compile("#{prefix}#{input}#{suffix}", :style => :compressed) # Compile compressed with prefix/suffix
+    output = compressed_result.gsub(/(#{prefix}|#{suffix})/, '') # Strip out prefix/suffix
+    
+    output
+  end
 end
