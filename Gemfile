a2a89def6f246e5ae90e8f89cb9b46f5bc7214f1
Johnathan Croom <johnathancroom@gmail.com>
2012-06-30 13:09:03 -0700

    Started visualization among other things
    - External CSS
    - Started visualization
    - Switched to JSON output
    - Catch and show errors

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin'
 gem 'sass'
+gem 'json'
--- web.rb
+++ web.rb
@@ -1,21 +1,47 @@
 require 'sinatra'
 require 'sass'
+require 'json'
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  input = 'lighten(#333, 10%)' # Input value
   input = params[:code]
   
-  if input != ''
-    prefix = 'body{color:'
-    suffix = '}'
-    
+  prefix = 'body{color:' # Arbitrary prefix
+  suffix = '}' # Arbitrary suffix
+  
+  begin
     compressed_result = Sass.compile("#{prefix}#{input}#{suffix}", :style => :compressed) # Compile compressed with prefix/suffix
-    output = compressed_result.gsub(/(#{prefix}|#{suffix})/, '') # Strip out prefix/suffix
-    
-    output
+  rescue => e # Capture errors
+    compressed_result = e.message # Display error
   end
+  
+  output = compressed_result.gsub(/(#{prefix}|#{suffix})/, '') # Strip out prefix/suffix
+  
+  # Check if color function
+  spaceless_input = input.gsub(/ /, '') # An input with no spaces
+  regex_color_function = /(^\w+\(#?\w+,(\d+.?\d%?)\)|^\w+\(#?\w+\))/
+  if regex_color_function =~ spaceless_input
+    spaceless_input.gsub(/\((?<function>#?\w+)/) {
+      @original_color = $1
+    }
+  end
+  
+  content_type :json # Return content type json
+  # Return json
+  { 
+    :output => output,
+    :original_color => (@original_color || '')
+  }.to_json
+end
+
+# Stylesheet compile/compress
+get '/stylesheets/:basename.css' do
+  @file = File.new("stylesheets/#{params[:basename]}.scss")
+  
+  last_modified @file.mtime
+  
+  scss @file.read, :style => :compressed
 end
